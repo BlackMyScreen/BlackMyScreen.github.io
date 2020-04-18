5b615caa67da0cd144d32049d36034011f09f7d9
CanRau <cansrau@gmail.com>
2020-04-18 03:20:02 -0500

    build: add hash, move all to root

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,55 @@
+import fs from "fs";
+import crypto from "crypto";
 import svelte from "rollup-plugin-svelte";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import del from "rollup-plugin-delete";
+import html, { makeHtmlAttributes } from "@rollup/plugin-html";
 import livereload from "rollup-plugin-livereload";
 import { terser } from "rollup-plugin-terser";
+import { minify } from "html-minifier-terser";
 
 const production = !process.env.ROLLUP_WATCH;
+
+// TODO: possibly inline everything?
+const template = async ({ attributes, files, bundle, publicPath, title }) => {
+  const scripts = (files.js || [])
+    .map(({ fileName, code, map }) => {
+      const hash = crypto
+        .createHash("sha512")
+        .update(code)
+        .digest("hex")
+        .slice(0, 25);
+      const hashedFileName = fileName.replace("[hash]", hash);
+      const attrs = makeHtmlAttributes(attributes.script);
+      const file = bundle[fileName];
+      file.fileName = hashedFileName;
+      delete bundle[fileName];
+      bundle[hashedFileName] = file;
+
+      return `<script defer src="${publicPath}${hashedFileName}"${attrs}></script>`;
+    })
+    .join("\n");
+
+  const links = (files.css || [])
+    .map(({ fileName }) => {
+      const attrs = makeHtmlAttributes(attributes.link);
+      return `<link href="${publicPath}${fileName}" rel="stylesheet"${attrs}>`;
+    })
+    .join("\n");
+
+  console.log({ title });
+
+  return minify(
+    fs
+      .readFileSync("./src/index.html")
+      .toString()
+      .replace("{{HTML_ATTRIBUTES}}", makeHtmlAttributes(attributes.html))
+      .replace("{{HTML_LINKS}}", links)
+      .replace("{{HTML_SCRIPTS}}", scripts),
+    { collapseWhitespace: true, minifyCSS: true, minifyJS: true }
+  );
+};
 
 export default {
   input: "src/main.js",
@@ -12,9 +57,10 @@ export default {
     sourcemap: true,
     format: "iife",
     name: "app",
-    file: "build/bundle.js",
+    file: "bundle-[hash].js",
   },
   plugins: [
+    del({ runOnce: true, targets: ["bundle-*", "index.html"] }),
     svelte({
       // enable run-time checks when not in production
       dev: !production,
@@ -43,6 +89,16 @@ export default {
     // Watch the `.` directory and refresh the
     // browser on changes when not in production
     !production && livereload("."),
+
+    // html({
+    //   template: "./src/index.html",
+    //   dest: "./build",
+    //   filename: "index.html",
+    //   inject: "head",
+    // }),
+    html({
+      template,
+    }),
 
     // If we're building for production (npm run build
     // instead of npm run dev), minify
