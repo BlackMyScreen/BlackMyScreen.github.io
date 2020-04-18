87515a740113b83781178c7c60e6fab9dab992e8
CanRau <cansrau@gmail.com>
2020-04-18 03:30:43 -0500

    build: update files

--- worker-imfeelinglucky.js
+++ worker-imfeelinglucky.js
@@ -1,14 +1,16 @@
 onmessage = function (e) {
   let imageData = e.data[2];
+  const width = e.data[0];
+  const height = e.data[1];
   for (let p = 0; p < imageData.data.length; p += 4) {
     const i = p / 4;
-    const x = i % e.data[0];
-    const y = (i / e.data[1]) >>> 0;
+    const x = i % width;
+    const y = (i / height) >>> 0;
 
     const t = performance.now();
 
-    const r = 64 + (128 * x) / e.data[0] + 64 * Math.sin(t / 1000);
-    const g = 64 + (128 * y) / e.data[1] + 64 * Math.cos(t / 1000);
+    const r = 64 + (128 * x) / width + 64 * Math.sin(t / 1000);
+    const g = 64 + (128 * y) / height + 64 * Math.cos(t / 1000);
     const b = 128;
 
     imageData.data[p + 0] = r;
