ecd025689e23a5edd54f8668c0c531605b485c16
CanRau <cansrau@gmail.com>
2020-04-18 23:18:11 -0500

    fix(worker): use var instead of let/const

--- worker-imfeelinglucky.js
+++ worker-imfeelinglucky.js
@@ -1,17 +1,17 @@
-onmessage = function (e) {
-  let imageData = e.data[2];
-  const width = e.data[0];
-  const height = e.data[1];
-  for (let p = 0; p < imageData.data.length; p += 4) {
-    const i = p / 4;
-    const x = i % width;
-    const y = (i / height) >>> 0;
+onmessage = function (event) {
+  var imageData = event.data[2];
+  var width = event.data[0];
+  var height = event.data[1];
+  for (var p = 0; p < imageData.data.length; p += 4) {
+    var i = p / 4;
+    var x = i % width;
+    var y = (i / height) >>> 0;
 
-    const t = performance.now();
+    var t = performance.now();
 
-    const r = 64 + (128 * x) / width + 64 * Math.sin(t / 1000);
-    const g = 64 + (128 * y) / height + 64 * Math.cos(t / 1000);
-    const b = 128;
+    var r = 64 + (128 * x) / width + 64 * Math.sin(t / 1000);
+    var g = 64 + (128 * y) / height + 64 * Math.cos(t / 1000);
+    var b = 128;
 
     imageData.data[p + 0] = r;
     imageData.data[p + 1] = g;
